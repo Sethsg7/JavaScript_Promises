673e588e212a0bca161f825cb7d54f1f8f80911f
Seth S. Green <sethsg7@gmail.com>
2023-03-09 09:37:10 -0600

    Promises

--- app.js
+++ app.js
@@ -16,9 +16,23 @@ function getList() {
 }
 
 // TODO: Handle the resolved or rejected states of the promise
+let para = document.getElementById("error");
+let ul = document.getElementById("list");
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
+getList()
+  .then((list) => {
+    list.forEach((name) => {
+      let li = document.createElement("li");
+      li.textContent = name;
+      ul.appendChild(li);
+    });
+  })
+  .catch((err) => {
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+para.textContent = err.message;
+  });
+
+
